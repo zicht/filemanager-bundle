d079d42ac1fcf450968447c5026aded4af438561
Gerard van Helden <gerard@zicht.nl>
2013-01-22 17:48:27 +0000

    added camelization of property names
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFileManagerBundle/trunk@15608 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/FileManagerBundle/Doctrine/PropertyHelper.php
+++ src/Zicht/Bundle/FileManagerBundle/Doctrine/PropertyHelper.php
@@ -6,17 +6,25 @@
 
 namespace Zicht\Bundle\FileManagerBundle\Doctrine;
 
+use Symfony\Component\Form\Util\PropertyPath;
+
 // TODO do proper inflection and detection of setters/getters
 class PropertyHelper
 {
+    public static function camelize($name)
+    {
+        return preg_replace_callback('/_([a-z])/', function($m) { return ucfirst($m[1]); }, $name);
+    }
+
+
     static function setValue($entity, $field, $value)
     {
-        $entity->{'set' . ucfirst($field)}($value);
+        $entity->{'set' . ucfirst(self::camelize($field))}($value);
     }
 
 
     static function getValue($entity, $field)
     {
-        return $entity->{'get' . ucfirst($field)}();
+        return $entity->{'get' . ucfirst(self::camelize($field))}();
     }
 }
