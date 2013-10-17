5379a1848e6d8864fc7d16359f5ac4e2be4303aa
Gerard van Helden <gerard@zicht.nl>
2013-10-17 12:42:54 +0000

    tweaked the test
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFileManagerBundle/trunk@26678 0f35748b-8228-47f0-bbea-889afbbd9a42

--- tests/ZichtTest/Bundle/FileManagerBundle/Twig/FileManagerExtensionTest.php
+++ tests/ZichtTest/Bundle/FileManagerBundle/Twig/FileManagerExtensionTest.php
@@ -7,15 +7,35 @@ namespace ZichtTest\Bundle\FileManagerBundle\Twig;
 
 class FileManagerExtensionTest extends \PHPUnit_Framework_TestCase
 {
-    function testExtension()
+    function testExtensionName()
     {
-        $fm = $this->getMockBuilder('Zicht\Bundle\FileManagerBundle\FileManager\FileManager')->disableOriginalConstructor()->getMock();
-        $ext = new \Zicht\Bundle\FileManagerBundle\Twig\FileManagerExtension($fm);
+        list($ext) = $this->getExtension();
 
         $this->assertEquals('zicht_filemanager', $ext->getName());
-        $this->assertArrayHasKey('file_url', $ext->getFunctions());
+    }
 
-        $fm->expects($this->once())->method('getFileUrl')->with(1, 2);
-        $ext->getFileUrl(1, 2);
+    public function getExtension()
+    {
+        $fm  = $this->getMockBuilder('Zicht\Bundle\FileManagerBundle\FileManager\FileManager')->disableOriginalConstructor()->getMock();
+        $ext = new \Zicht\Bundle\FileManagerBundle\Twig\FileManagerExtension($fm);
+
+        return array($ext, $fm);
+    }
+
+
+    function testFileUrlIsATwigFunction()
+    {
+        list($ext) = $this->getExtension();
+        $this->assertArrayHasKey('file_url', $ext->getFunctions());
+    }
+
+    function testGetFileUrlDelegatesToFileManagerGetFileUrl()
+    {
+        list($ext, $fm) = $this->getExtension();
+
+        $fm->expects($this->once())->method('getFileUrl')->with(1, 2)->will($this->returnValue('foo'));
+        $ret = $ext->getFileUrl(1, 2);
+
+        $this->assertEquals('foo', $ret);
     }
 }
