4c5f4b105c81481a09a8838ec7bb43c59e0893c9
Oskar van Velden <oskar@zicht.nl>
2013-10-24 14:37:53 +0000

    some basis tests for the FileTypeSubscriber
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFileManagerBundle/branches/save-images-pre-validation@27064 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ tests/ZichtTest/Bundle/FileManagerBundle/Form/FileTypeSubscriberTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Oskar van Velden <oskar@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+  */
+
+namespace ZichtTest\Bundle\FileManagerBundle\Form\Transformer;
+
+use Symfony\Component\Form\FormEvents;
+
+class FileTypeSubscriberTestEntity
+{
+    public $foo;
+}
+
+class FileTypeSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    function setUp()
+    {
+        $this->fm = $this->getMockBuilder('Zicht\Bundle\FileManagerBundle\FileManager\FileManager')
+            ->setMethods(array('getFilePath'))
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    function testSubscriberCreation()
+    {
+        $entity = new FileTypeSubscriberTestEntity();
+        $field  = 'inputField';
+        $subscriber = new \Zicht\Bundle\FileManagerBundle\Form\FileTypeSubscriber($this->fm, $entity, $field);
+    }
+
+    function testGetEventsToGetFullCoverageHarHarHar()
+    {
+        $events = \Zicht\Bundle\FileManagerBundle\Form\FileTypeSubscriber::getSubscribedEvents();
+
+        $this->assertTrue(is_array($events));
+        $this->assertArrayHasKey(FormEvents::POST_SET_DATA, $events);
+        $this->assertArrayHasKey(FormEvents::BIND, $events);
+    }
+
+
+}
