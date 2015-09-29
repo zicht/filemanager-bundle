211b7626f62a44a1a260c0fdf57258435c0e593a
Rik van der Kemp <rik@zicht.nl>
2015-09-29 10:35:17 +0000

    first start of editing images with moxiemanager
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFileManagerBundle/branches/4.x@69908 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ src/Zicht/Bundle/FileManagerBundle/Controller/ImagineController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @author Rik van der Kemp <rik@zicht.nl>
+ * @copyright Zicht Online <http://www.zicht.nl>
+ */
+
+namespace Zicht\Bundle\FileManagerBundle\Controller;
+
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Provides some methods to communicate with Imagine e.g. Clear cache for a file
+ *
+ * @package Zicht\Bundle\FileManagerBundle\Controller
+ */
+class ImagineController extends Controller
+{
+    /**
+     * Remove thumbnail for given path
+     *
+     * @Route("/clear-thumbnail")
+     *
+     * @return JsonResponse
+     */
+    public function clearThumbnailAction(Request $request)
+    {
+        $path = $request->get('path');
+        $filter = $request->get('filter');
+        $response = [
+            'error' => false,
+            'success' => false
+        ];
+
+        if ($path && $filter) {
+            /** @var CacheManager $cacheManager */
+            $cacheManager = $this->get('liip_imagine.cache.manager');
+            $cacheManager->remove($path, $filter);
+            $cacheManager->getBrowserPath($path, $filter);
+            $response['success'] = true;
+            $response['url']  = $cacheManager->generateUrl($path, $filter);
+        } else {
+            $response['error'] = 'No "path" or "filter" provided';
+        }
+
+        return new JsonResponse($response);
+    }
+}
