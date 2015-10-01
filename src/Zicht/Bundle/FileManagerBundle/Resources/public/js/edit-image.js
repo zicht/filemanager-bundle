456087e3e19c611dbc94ba43fab4244ad7aca700
Rik van der Kemp <rik@zicht.nl>
2015-10-01 12:37:52 +0000

    Strips of request params from FilePath to prevent issues with long filenames. This happened with some Bynder images where the image path was preceded by ?authtoken=aaaaaa&some=2222222&foo=bar etc. Also fixes issue with the edit button and .jpeg files
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFileManagerBundle/branches/4.x@70015 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/FileManagerBundle/Resources/public/js/edit-image.js
+++ src/Zicht/Bundle/FileManagerBundle/Resources/public/js/edit-image.js
@@ -7,30 +7,32 @@ $(function () {
             $imagePreview = $(this).find('.js-moxiemanager-edit-image__image-preview'),
             fileUrl = $trigger.data('file-url');
 
-        fileUrl = fileUrl.split('?')[0];
+        if (fileUrl) {
+            fileUrl = fileUrl.split('?')[0];
 
-        $trigger.click(function (e) {
-            e.preventDefault();
+            $trigger.click(function (e) {
+                e.preventDefault();
 
-            moxman.edit({
-                path: fileUrl,
-                onsave: function (args) {
-                    $.ajax({
-                        url: '/admin/filemanager/clear-thumbnail',
-                        data: {
-                            path: fileUrl,
-                            filter: 'cms'
-                        },
-                        success: function (response) {
-                            // The response URL can not be refreshed somehow,
-                            // Therefor, use the 'original' image as the source and force a
-                            // browser reload by adding the timestamp. TODO find a way to do this better
-                            $imagePreview.find('img').removeAttr("src").attr('src',  args.file.url + '?timestamp=' + new Date().getTime());
-                            $imagePreview.find('img').css('width', '50px');
-                        }
-                    });
-                }
+                moxman.edit({
+                    path: fileUrl,
+                    onsave: function (args) {
+                        $.ajax({
+                            url: '/admin/filemanager/clear-thumbnail',
+                            data: {
+                                path: fileUrl,
+                                filter: 'cms'
+                            },
+                            success: function (response) {
+                                // The response URL can not be refreshed somehow,
+                                // Therefor, use the 'original' image as the source and force a
+                                // browser reload by adding the timestamp. TODO find a way to do this better
+                                $imagePreview.find('img').removeAttr("src").attr('src', args.file.url + '?timestamp=' + new Date().getTime());
+                                $imagePreview.find('img').css('width', '50px');
+                            }
+                        });
+                    }
+                });
             });
-        });
+        }
     });
 });
